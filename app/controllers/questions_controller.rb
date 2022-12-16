42b37e473d0f5fe8f869acef11be68e1e610b6d3
Alban de La Rochefordiere <dlralban@gmail.com>
2022-12-16 16:50:20 +0100

    rails stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question == "I'm going to work"
+      @answer = "Great!"
+    elsif @question.end_with?('?')
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work"
+    end
+  end
+end
